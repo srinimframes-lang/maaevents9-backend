2af4059110106a8c0fe7e0cbcbafd93067869496
srinimframes-lang <srinimframes@gmail.com>
2026-06-26 12:22:32 +0530

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
+  console.error("FULL ERROR:", err);
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
     success: false,
